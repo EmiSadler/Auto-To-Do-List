8b3bfc561378c6e375845520068b2219420e316c
EmiSadler <Emily@sadler.gg>
2026-08-05 16:13:38 +0100

    added connectGoogleAccount function to trigger login and save token

--- background.js
+++ background.js
@@ -3,3 +3,15 @@ function openPanel() {
     chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true});
     console.log("background loaded")
 }
+
+function connectGoogleAccount() {
+    chrome.identity.getAuthToken({ interactive: true }, (token) => {
+        if (chrome.runtime.lastError) {
+            console.error("Auth failed", chrome.runtime.lastError);
+            return;
+        }
+        chrome.storage.local.set({ authToken: token }, () => {
+
+        });
+    });
+}
