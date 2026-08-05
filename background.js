f8b990a79237682f6df8e0dccff647c3be7c3849
EmiSadler <Emily@sadler.gg>
2026-08-05 16:40:00 +0100

    console.log statements for debugging

--- background.js
+++ background.js
@@ -6,9 +6,11 @@ function openPanel() {
 
 function connectGoogleAccount() {
     chrome.identity.getAuthToken({ interactive: true }, (token) => {
+        console.log("token is:", token);
         if (chrome.runtime.lastError) {
-            console.error("Auth failed", chrome.runtime.lastError);
-            return;
+            console.log("error message is:", chrome.runtime.lastError.message);
+            //console.error("Auth failed", chrome.runtime.lastError);
+            //return;
         }
         chrome.storage.local.set({ authToken: token }, () => {
 
