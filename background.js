9babb825e4a25364a943a3f4b1ce76ec90690be6
EmiSadler <Emily@sadler.gg>
2026-08-21 14:37:21 +0100

    added the fetchTodaysEvents function connecting to GoogleAPI

--- background.js
+++ background.js
@@ -26,5 +26,28 @@ function checkAuthStatus() {
     });
 }
 
+function fetchTodaysEvents() {
+    chrome.storage.local.get('authToken', ({ authToken }) => {
+        const now = new Date();
+        const startOfDay = new Date(now,getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0);
+        const endOfDay = new Date(now.getFullYear(), now,getMonth(), now.getDate(), 23, 59. 59);
+
+        const url = 'https://www.googleapis.com/calendar/v3/calendars/primary/events?timeMin=${startOfDay.ISOString()}&timeMax=${endOfDay.toISOString()}&singleEvents=true&orderBy=startTime';
+
+        fetch(url, {
+            headers: {
+                Authorization: 'Bearer ${authToken}'
+            }
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log("Today's events:", data.items);
+        })
+        .catch((error) => {
+            console.error("Fetch failed:", error);
+        });
+    });
+}
+
 checkAuthStatus();
 openPanel();
